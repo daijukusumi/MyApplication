ef1b021c6ec6e578bd5bbaadac747d33fa9b0bef
daiju.kusumi <daiju.jutp@gmail.com>
2017-09-11 03:19:09 +0900

    Humanクラスの定義

--- app/src/main/java/jp/techacademy/kusumi/daiju/myapplication/MainActivity.java
+++ app/src/main/java/jp/techacademy/kusumi/daiju/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package jp.techacademy.kusumi.daiju.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Human human = new Human("ボブ", 24,"サッカー");     // 名前をポチ、年齢3歳で、Dogのインスタンスを作る
+
+        human.say();
+        human.think();
     }
+
 }
